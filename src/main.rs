2e1b64da36b63c857562e671b507b0b1d3649499
ziyoung <dengzhiyang123@126.com>
2024-07-30 14:59:25 +0800

    Regex simultaneous matches

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,57 @@
-use std::{io::Read, time::Duration};
+use std::{env, time::Duration};
 
 use once_cell::sync::Lazy;
 use regex::Regex;
-use tikv_jemallocator::Jemalloc;
-use tokio::fs;
-
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
+use tokio::{fs, task::JoinSet};
 
 static REG: Lazy<Regex> = Lazy::new(|| Regex::new(r#"(Object\.\w+)"#).unwrap());
 
-async fn dump_prof(id: usize) -> anyhow::Result<()> {
-    let mut prof_ctl = jemalloc_pprof::PROF_CTL.as_ref().unwrap().lock().await;
-    let mut file = prof_ctl
-        .dump()
-        .map_err(|e| anyhow::anyhow!("load .prof file failed: {}", e))?;
-    let mut buf = Vec::with_capacity(1024 * 16);
-    file.read_to_end(&mut buf)
-        .map_err(|e| anyhow::anyhow!("write prof file to buffer failed: {}", e))?;
-    let name = format!("./profile/regex-captures-iter-{}.prof", id);
-    fs::write(name, buf)
-        .await
-        .map_err(|e| anyhow::anyhow!("write .prof file failed: {}", e))
-}
-
 async fn memory_leak_demo() -> Vec<u8> {
-    let text = reqwest::get("https://cdnjs.cloudflare.com/ajax/libs/vue/3.4.34/vue.cjs.js")
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
-    let mut buf = Vec::from(text.as_bytes());
-    for (i, caps) in REG.captures_iter(&text).enumerate() {
-        println!("process text: {}", i + 1);
-        tokio::time::sleep(Duration::from_millis(50)).await;
-        let m = caps.get(0).unwrap();
-        let start = m.start();
-        let end = m.end();
-        let text = &text[start..end];
-        buf.extend(text.as_bytes());
+    let file = fs::read("./vue.js").await.unwrap();
+    let mut buf = file.clone();
+    let text = String::from_utf8(file).unwrap();
+
+    if env::var("SYNC_REG_MATCH").is_ok() {
+        let positions = REG
+            .captures_iter(&text)
+            .map(|caps| {
+                let m = caps.get(0).unwrap();
+                (m.start(), m.end())
+            })
+            .collect::<Vec<_>>();
+        for (start, end) in positions {
+            buf.extend(text[start..end].as_bytes());
+        }
+    } else {
+        for caps in REG.captures_iter(&text) {
+            tokio::time::sleep(Duration::from_millis(5)).await;
+            let m = caps.get(0).unwrap();
+            let start = m.start();
+            let end = m.end();
+            let text = &text[start..end];
+            buf.extend(text.as_bytes());
+        }
     }
     buf
 }
 
-async fn task() {
-    for _ in 0..50 {
-        let buf = memory_leak_demo().await;
-        let s = String::from_utf8(buf).unwrap_or_default();
-        println!("string length: {}", s.len());
-    }
-}
-
 #[tokio::main]
 async fn main() {
-    tokio::spawn(async {
-        dump_prof(0).await.unwrap();
-        for i in 0..5 {
-            task().await;
-            tokio::time::sleep(Duration::from_secs(10)).await;
-            println!("write prof");
-            dump_prof(i + 1).await.unwrap();
-        }
-    })
-    .await
-    .unwrap();
-    println!("done");
+    {
+        tokio::spawn(async {
+            for i in 0..20 {
+                let mut set = JoinSet::new();
+                for _ in 0..10_000 {
+                    set.spawn(memory_leak_demo());
+                }
+                while let Some(_res) = set.join_next().await {}
+                println!("task {} done", i);
+                tokio::time::sleep(Duration::from_millis(10)).await;
+            }
+        })
+        .await
+        .unwrap();
+    }
+    println!("use CTRL + C to exit");
+    tokio::signal::ctrl_c().await.unwrap();
 }
